2424008feb6bdea0064022ee3668c55d877ca654
tarek abdelhak <tarekroja123@gmail.com>
2022-02-13 11:52:59 +0200

    rename the font-end directory

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const {application} = require("express");
 // instantiate an app instance and use the required dependencies
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("website"));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
